e0db1ec6961b58e5b6423b3ddf6ff484d08c2422
Mark Sobkow <mark.sobkow@gmail.com>
2026-07-27 14:54:34 -0600

    2026-07-27 14:55 CST Work in progress that actually runs the CFBam code production without getting stuck in an infinite loop.

---
+++ src/main/java/server/markhome/mcf/v3_1/cfbam/cfbamjavafx/CFBamJavaFXSchemaDefEltTabPane.java
@@ -0,0 +1,414 @@
+// Description: Java 25 JavaFX Element TabPane implementation for SchemaDef.
+
+/*
+ *	server.markhome.mcf.CFBam
+ *
+ *	Copyright (c) 2016-2026 Mark Stephen Sobkow
+ *	
+ *	Mark's Code Fractal CFBam 3.1 Business Application Model
+ *	
+ *	Copyright 2016-2026 Mark Stephen Sobkow
+ *	
+ *	This file is part of Mark's Code Fractal CFBam.
+ *	
+ *	Mark's Code Fractal CFBam is available under dual commercial license from
+ *	Mark Stephen Sobkow, or under the terms of the GNU General Public License,
+ *	Version 3 or later with classpath and static linking exceptions.
+ *	
+ *	As a special exception, Mark Sobkow gives you permission to link this library
+ *	with independent modules to produce an executable, provided that none of them
+ *	conflict with the intent of the GPLv3; that is, you are not allowed to invoke
+ *	the methods of this library from non-GPLv3-compatibly licensed code. You may not
+ *	implement an LPGLv3 "wedge" to try to bypass this restriction. That said, code which
+ *	does not rely on this library is free to specify whatever license its authors decide
+ *	to use. Mark Sobkow specifically rejects the infectious nature of the GPLv3, and
+ *	considers the mere act of including GPLv3 modules in an executable to be perfectly
+ *	reasonable given tools like modern Java's single-jar deployment options.
+ *	
+ *	Mark's Code Fractal CFBam is free software: you can redistribute it and/or
+ *	modify it under the terms of the GNU General Public License as published by
+ *	the Free Software Foundation, either version 3 of the License, or
+ *	(at your option) any later version.
+ *	
+ *	Mark's Code Fractal CFBam is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *	GNU General Public License for more details.
+ *	
+ *	You should have received a copy of the GNU General Public License
+ *	along with Mark's Code Fractal CFBam.  If not, see <https://www.gnu.org/licenses/>.
+ *	
+ *	If you wish to modify and use this code without publishing your changes,
+ *	or integrate it with proprietary code, please contact Mark Stephen Sobkow
+ *	for a commercial license at mark.sobkow@gmail.com
+ */
+
+package server.markhome.mcf.v3_1.cfbam.cfbamjavafx;
+
+import java.math.*;
+import java.time.*;
+import java.text.*;
+import java.util.*;
+import java.util.List;
+import javafx.scene.Parent;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.ScrollPane.ScrollBarPolicy;
+import server.markhome.mcf.v3_1.cflib.*;
+import server.markhome.mcf.v3_1.cflib.dbutil.*;
+import server.markhome.mcf.v3_1.cflib.inz.Inz;
+import server.markhome.mcf.v3_1.cflib.javafx.*;
+import org.apache.commons.codec.binary.Base64;
+import server.markhome.mcf.v3_1.cfsec.cfsec.*;
+import server.markhome.mcf.v3_1.cfint.cfint.*;
+import server.markhome.mcf.v3_1.cfbam.cfbam.*;
+import server.markhome.mcf.v3_1.cfsec.cfsecobj.*;
+import server.markhome.mcf.v3_1.cfint.cfintobj.*;
+import server.markhome.mcf.v3_1.cfbam.cfbamobj.*;
+
+/**
+ *	CFBamJavaFXSchemaDefEltTabPane JavaFX Element TabPane implementation
+ *	for SchemaDef.
+ */
+public class CFBamJavaFXSchemaDefEltTabPane
+extends CFTabPane
+implements ICFBamJavaFXSchemaDefPaneCommon
+{
+	protected ICFFormManager cfFormManager = null;
+	protected ICFBamJavaFXSchema javafxSchema = null;
+	protected boolean javafxIsInitializing = true;
+	public final String LABEL_TabComponentsTablesList = "Optional Components Table Definitions";
+	protected CFTab tabComponentsTables = null;
+	public final String LABEL_TabComponentsTypesList = "Optional Components Type Definitions";
+	protected CFTab tabComponentsTypes = null;
+	public final String LABEL_TabComponentsSchemaRefsList = "Optional Components Schema References";
+	protected CFTab tabComponentsSchemaRefs = null;
+	public final String LABEL_TabComponentsTweaksList = "Optional Components Schema Tweaks";
+	protected CFTab tabComponentsTweaks = null;
+	public final String LABEL_TabComponentsRolesList = "Optional Components Schema Roles";
+	protected CFTab tabComponentsRoles = null;
+	protected CFBorderPane tabViewComponentsTablesListPane = null;
+	protected CFBorderPane tabViewComponentsTypesListPane = null;
+	protected CFBorderPane tabViewComponentsSchemaRefsListPane = null;
+	protected CFBorderPane tabViewComponentsTweaksListPane = null;
+	protected CFBorderPane tabViewComponentsRolesListPane = null;
+
+	public CFBamJavaFXSchemaDefEltTabPane( ICFFormManager formManager, ICFBamJavaFXSchema argSchema, ICFBamSchemaDefObj argFocus ) {
+		super();
+		final String S_ProcName = "construct-schema-focus";
+		if( formManager == null ) {
+			throw new CFLibNullArgumentException( getClass(),
+				S_ProcName,
+				1,
+				"formManager" );
+		}
+		cfFormManager = formManager;
+		if( argSchema == null ) {
+			throw new CFLibNullArgumentException( getClass(),
+				S_ProcName,
+				2,
+				"argSchema" );
+		}
+		// argFocus is optional; focus may be set later during execution as
+		// conditions of the runtime change.
+		javafxSchema = argSchema;
+		setJavaFXFocusAsSchemaDef( argFocus );
+		// Wire the newly constructed Panes/Tabs to this TabPane
+		tabComponentsTables = new CFTab();
+		tabComponentsTables.setText( LABEL_TabComponentsTablesList );
+		tabComponentsTables.setContent( getTabViewComponentsTablesListPane() );
+		getTabs().add( tabComponentsTables );
+		tabComponentsTypes = new CFTab();
+		tabComponentsTypes.setText( LABEL_TabComponentsTypesList );
+		tabComponentsTypes.setContent( getTabViewComponentsTypesListPane() );
+		getTabs().add( tabComponentsTypes );
+		tabComponentsSchemaRefs = new CFTab();
+		tabComponentsSchemaRefs.setText( LABEL_TabComponentsSchemaRefsList );
+		tabComponentsSchemaRefs.setContent( getTabViewComponentsSchemaRefsListPane() );
+		getTabs().add( tabComponentsSchemaRefs );
+		tabComponentsTweaks = new CFTab();
+		tabComponentsTweaks.setText( LABEL_TabComponentsTweaksList );
+		tabComponentsTweaks.setContent( getTabViewComponentsTweaksListPane() );
+		getTabs().add( tabComponentsTweaks );
+		tabComponentsRoles = new CFTab();
+		tabComponentsRoles.setText( LABEL_TabComponentsRolesList );
+		tabComponentsRoles.setContent( getTabViewComponentsRolesListPane() );
+		getTabs().add( tabComponentsRoles );
+		javafxIsInitializing = false;
+	}
+
+	public ICFFormManager getCFFormManager() {
+		return( cfFormManager );
+	}
+
+	public void setCFFormManager( ICFFormManager value ) {
+		final String S_ProcName = "setCFFormManager";
+		if( value == null ) {
+			throw new CFLibNullArgumentException( getClass(),
+				S_ProcName,
+				1,
+				"value" );
+		}
+		cfFormManager = value;
+	}
+
+	public ICFBamJavaFXSchema getJavaFXSchema() {
+		return( javafxSchema );
+	}
+
+	public void setJavaFXFocus( ICFLibAnyObj value ) {
+		final String S_ProcName = "setJavaFXFocus";
+		if( ( value == null ) || ( value instanceof ICFBamSchemaDefObj ) ) {
+			super.setJavaFXFocus( value );
+		}
+		else {
+			throw new CFLibUnsupportedClassException( getClass(),
+				S_ProcName,
+				"value",
+				value,
+				"ICFBamSchemaDefObj" );
+		}
+	}
+
+	public void setJavaFXFocusAsSchemaDef( ICFBamSchemaDefObj value ) {
+		setJavaFXFocus( value );
+	}
+
+	public ICFBamSchemaDefObj getJavaFXFocusAsSchemaDef() {
+		return( (ICFBamSchemaDefObj)getJavaFXFocus() );
+	}
+
+	protected class RefreshComponentsTablesList
+	implements ICFRefreshCallback
+	{
+		public RefreshComponentsTablesList() {
+		}
+
+		public void refreshMe() {
+			Collection<ICFBamTableObj> dataCollection;
+			ICFBamSchemaDefObj focus = (ICFBamSchemaDefObj)getJavaFXFocusAsSchemaDef();
+			if( focus != null ) {
+				dataCollection = focus.getOptionalComponentsTables( javafxIsInitializing );
+			}
+			else {
+				dataCollection = null;
+			}
+			CFBorderPane pane = getTabViewComponentsTablesListPane();
+			ICFBamJavaFXTablePaneList jpList = (ICFBamJavaFXTablePaneList)pane;
+			jpList.setJavaFXDataCollection( dataCollection );
+		}
+	}
+
+	public CFBorderPane getTabViewComponentsTablesListPane() {
+		if( tabViewComponentsTablesListPane == null ) {
+			ICFBamSchemaDefObj focus = (ICFBamSchemaDefObj)getJavaFXFocusAsSchemaDef();
+			Collection<ICFBamTableObj> dataCollection;
+			if( focus != null ) {
+				dataCollection = focus.getOptionalComponentsTables( javafxIsInitializing );
+			}
+			else {
+				dataCollection = null;
+			}
+			ICFBamSchemaDefObj javafxContainer;
+			if( ( focus != null ) && ( focus instanceof ICFBamSchemaDefObj ) ) {
+				javafxContainer = (ICFBamSchemaDefObj)focus;
+			}
+			else {
+				javafxContainer = null;
+			}
+			tabViewComponentsTablesListPane = javafxSchema.getTableFactory().newListPane( cfFormManager, javafxContainer, null, dataCollection, new RefreshComponentsTablesList(), false );
+		}
+		return( tabViewComponentsTablesListPane );
+	}
+
+	protected class RefreshComponentsTypesList
+	implements ICFRefreshCallback
+	{
+		public RefreshComponentsTypesList() {
+		}
+
+		public void refreshMe() {
+			Collection<ICFBamValueObj> dataCollection;
+			ICFBamSchemaDefObj focus = (ICFBamSchemaDefObj)getJavaFXFocusAsSchemaDef();
+			if( focus != null ) {
+				dataCollection = focus.getOptionalComponentsTypes( javafxIsInitializing );
+			}
+			else {
+				dataCollection = null;
+			}
+			CFBorderPane pane = getTabViewComponentsTypesListPane();
+			ICFBamJavaFXValuePaneList jpList = (ICFBamJavaFXValuePaneList)pane;
+			jpList.setJavaFXDataCollection( dataCollection );
+		}
+	}
+
+	public CFBorderPane getTabViewComponentsTypesListPane() {
+		if( tabViewComponentsTypesListPane == null ) {
+			ICFBamSchemaDefObj focus = (ICFBamSchemaDefObj)getJavaFXFocusAsSchemaDef();
+			Collection<ICFBamValueObj> dataCollection;
+			if( focus != null ) {
+				dataCollection = focus.getOptionalComponentsTypes( javafxIsInitializing );
+			}
+			else {
+				dataCollection = null;
+			}
+			ICFBamScopeObj javafxContainer;
+			if( ( focus != null ) && ( focus instanceof ICFBamScopeObj ) ) {
+				javafxContainer = (ICFBamScopeObj)focus;
+			}
+			else {
+				javafxContainer = null;
+			}
+			tabViewComponentsTypesListPane = javafxSchema.getValueFactory().newListPane( cfFormManager, javafxContainer, null, dataCollection, new RefreshComponentsTypesList(), true );
+		}
+		return( tabViewComponentsTypesListPane );
+	}
+
+	protected class RefreshComponentsSchemaRefsList
+	implements ICFRefreshCallback
+	{
+		public RefreshComponentsSchemaRefsList() {
+		}
+
+		public void refreshMe() {
+			Collection<ICFBamSchemaRefObj> dataCollection;
+			ICFBamSchemaDefObj focus = (ICFBamSchemaDefObj)getJavaFXFocusAsSchemaDef();
+			if( focus != null ) {
+				dataCollection = focus.getOptionalComponentsSchemaRefs( javafxIsInitializing );
+			}
+			else {
+				dataCollection = null;
+			}
+			CFBorderPane pane = getTabViewComponentsSchemaRefsListPane();
+			ICFBamJavaFXSchemaRefPaneList jpList = (ICFBamJavaFXSchemaRefPaneList)pane;
+			jpList.setJavaFXDataCollection( dataCollection );
+		}
+	}
+
+	public CFBorderPane getTabViewComponentsSchemaRefsListPane() {
+		if( tabViewComponentsSchemaRefsListPane == null ) {
+			ICFBamSchemaDefObj focus = (ICFBamSchemaDefObj)getJavaFXFocusAsSchemaDef();
+			Collection<ICFBamSchemaRefObj> dataCollection;
+			if( focus != null ) {
+				dataCollection = focus.getOptionalComponentsSchemaRefs( javafxIsInitializing );
+			}
+			else {
+				dataCollection = null;
+			}
+			ICFBamSchemaDefObj javafxContainer;
+			if( ( focus != null ) && ( focus instanceof ICFBamSchemaDefObj ) ) {
+				javafxContainer = (ICFBamSchemaDefObj)focus;
+			}
+			else {
+				javafxContainer = null;
+			}
+			tabViewComponentsSchemaRefsListPane = javafxSchema.getSchemaRefFactory().newListPane( cfFormManager, javafxContainer, null, dataCollection, new RefreshComponentsSchemaRefsList(), true );
+		}
+		return( tabViewComponentsSchemaRefsListPane );
+	}
+
+	protected class RefreshComponentsTweaksList
+	implements ICFRefreshCallback
+	{
+		public RefreshComponentsTweaksList() {
+		}
+
+		public void refreshMe() {
+			Collection<ICFBamTweakObj> dataCollection;
+			ICFBamSchemaDefObj focus = (ICFBamSchemaDefObj)getJavaFXFocusAsSchemaDef();
+			if( focus != null ) {
+				dataCollection = focus.getOptionalComponentsTweaks( javafxIsInitializing );
+			}
+			else {
+				dataCollection = null;
+			}
+			CFBorderPane pane = getTabViewComponentsTweaksListPane();
+			ICFBamJavaFXTweakPaneList jpList = (ICFBamJavaFXTweakPaneList)pane;
+			jpList.setJavaFXDataCollection( dataCollection );
+		}
+	}
+
+	public CFBorderPane getTabViewComponentsTweaksListPane() {
+		if( tabViewComponentsTweaksListPane == null ) {
+			ICFBamSchemaDefObj focus = (ICFBamSchemaDefObj)getJavaFXFocusAsSchemaDef();
+			Collection<ICFBamTweakObj> dataCollection;
+			if( focus != null ) {
+				dataCollection = focus.getOptionalComponentsTweaks( javafxIsInitializing );
+			}
+			else {
+				dataCollection = null;
+			}
+			ICFBamScopeObj javafxContainer;
+			if( ( focus != null ) && ( focus instanceof ICFBamScopeObj ) ) {
+				javafxContainer = (ICFBamScopeObj)focus;
+			}
+			else {
+				javafxContainer = null;
+			}
+			tabViewComponentsTweaksListPane = javafxSchema.getTweakFactory().newListPane( cfFormManager, javafxContainer, null, dataCollection, new RefreshComponentsTweaksList(), false );
+		}
+		return( tabViewComponentsTweaksListPane );
+	}
+
+	protected class RefreshComponentsRolesList
+	implements ICFRefreshCallback
+	{
+		public RefreshComponentsRolesList() {
+		}
+
+		public void refreshMe() {
+			Collection<ICFBamSchemaRoleObj> dataCollection;
+			ICFBamSchemaDefObj focus = (ICFBamSchemaDefObj)getJavaFXFocusAsSchemaDef();
+			if( focus != null ) {
+				dataCollection = focus.getOptionalComponentsRoles( javafxIsInitializing );
+			}
+			else {
+				dataCollection = null;
+			}
+			CFBorderPane pane = getTabViewComponentsRolesListPane();
+			ICFBamJavaFXSchemaRolePaneList jpList = (ICFBamJavaFXSchemaRolePaneList)pane;
+			jpList.setJavaFXDataCollection( dataCollection );
+		}
+	}
+
+	public CFBorderPane getTabViewComponentsRolesListPane() {
+		if( tabViewComponentsRolesListPane == null ) {
+			ICFBamSchemaDefObj focus = (ICFBamSchemaDefObj)getJavaFXFocusAsSchemaDef();
+			Collection<ICFBamSchemaRoleObj> dataCollection;
+			if( focus != null ) {
+				dataCollection = focus.getOptionalComponentsRoles( javafxIsInitializing );
+			}
+			else {
+				dataCollection = null;
+			}
+			ICFBamSchemaDefObj javafxContainer;
+			if( ( focus != null ) && ( focus instanceof ICFBamSchemaDefObj ) ) {
+				javafxContainer = (ICFBamSchemaDefObj)focus;
+			}
+			else {
+				javafxContainer = null;
+			}
+			tabViewComponentsRolesListPane = javafxSchema.getSchemaRoleFactory().newListPane( cfFormManager, javafxContainer, null, dataCollection, new RefreshComponentsRolesList(), false );
+		}
+		return( tabViewComponentsRolesListPane );
+	}
+
+	public void setPaneMode( CFPane.PaneMode value ) {
+		CFPane.PaneMode oldMode = getPaneMode();
+		super.setPaneMode( value );
+		if( tabViewComponentsTablesListPane != null ) {
+			((ICFBamJavaFXTablePaneCommon)tabViewComponentsTablesListPane).setPaneMode( value );
+		}
+		if( tabViewComponentsTypesListPane != null ) {
+			((ICFBamJavaFXValuePaneCommon)tabViewComponentsTypesListPane).setPaneMode( value );
+		}
+		if( tabViewComponentsSchemaRefsListPane != null ) {
+			((ICFBamJavaFXSchemaRefPaneCommon)tabViewComponentsSchemaRefsListPane).setPaneMode( value );
+		}
+		if( tabViewComponentsTweaksListPane != null ) {
+			((ICFBamJavaFXTweakPaneCommon)tabViewComponentsTweaksListPane).setPaneMode( value );
+		}
+		if( tabViewComponentsRolesListPane != null ) {
+			((ICFBamJavaFXSchemaRolePaneCommon)tabViewComponentsRolesListPane).setPaneMode( value );
+		}
+	}
+}
